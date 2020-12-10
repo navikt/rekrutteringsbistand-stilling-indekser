744b1f495133df4642eec2aa813019317af05a8a
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-12-10 10:48:17 +0100

    Legg inn logging med logback

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.javalin:javalin:3.12.0")
     implementation("org.slf4j:slf4j-simple:1.7.30")
-
+    implementation("net.logstash.logback:logstash-logback-encoder:6.3")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
