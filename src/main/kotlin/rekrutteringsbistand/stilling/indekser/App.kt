1d5e026625c19a28764e44cf3b642d816a5c0d34
André Mossige <mossige.a@gmail.com>
2020-12-10 08:28:21 +0100

    Legg på ingress og fjern base path

--- src/main/kotlin/rekrutteringsbistand/stilling/indekser/App.kt
+++ src/main/kotlin/rekrutteringsbistand/stilling/indekser/App.kt
@@ -4,11 +4,10 @@ import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.get
 
 fun main() {
-    val basePath = "/rekrutteringsbistand-stilling-indekser"
     val app = Javalin.create().start(8222)
 
     app.routes {
-        get("$basePath/internal/isAlive") { ctx -> ctx.status(200) }
-        get("$basePath/internal/isReady") { ctx -> ctx.status(200) }
+        get("/internal/isAlive") { ctx -> ctx.status(200) }
+        get("/internal/isReady") { ctx -> ctx.status(200) }
     }
 }
