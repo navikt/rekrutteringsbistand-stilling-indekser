22ae860ea79582ef6197f018497955d3b4087e4a
André Mossige <mossige.a@gmail.com>
2021-01-08 13:22:41 +0100

    Legg til timestamp i indeksnavn

--- src/main/kotlin/rekrutteringsbistand/stilling/indekser/elasticsearch/ElasticSearchService.kt
+++ src/main/kotlin/rekrutteringsbistand/stilling/indekser/elasticsearch/ElasticSearchService.kt
@@ -4,18 +4,26 @@ import org.elasticsearch.client.RequestOptions
 import org.elasticsearch.client.RestHighLevelClient
 import org.elasticsearch.client.indices.CreateIndexRequest
 import org.elasticsearch.client.indices.GetIndexRequest
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 const val stillingIndeks: String = "stilling"
 
 class ElasticSearchService(private val esClient: RestHighLevelClient) {
 
     fun opprettIndeksHvisDenIkkeFinnes() {
-        val getIndexRequest = GetIndexRequest(stillingIndeks)
+        val indeksNavn = indeksNavnMedTimestamp()
+        val getIndexRequest = GetIndexRequest(indeksNavn)
         val indeksFinnes = esClient.indices().exists(getIndexRequest, RequestOptions.DEFAULT)
 
         if (!indeksFinnes) {
-            val request = CreateIndexRequest(stillingIndeks)
+            val request = CreateIndexRequest(indeksNavn)
             esClient.indices().create(request, RequestOptions.DEFAULT)
         }
     }
+}
+
+private fun indeksNavnMedTimestamp(): String {
+    val dateTimeFormat = DateTimeFormatter.ofPattern("_yyyyMMdd_HHmmss")
+    return stillingIndeks + LocalDateTime.now().format(dateTimeFormat)
 }
