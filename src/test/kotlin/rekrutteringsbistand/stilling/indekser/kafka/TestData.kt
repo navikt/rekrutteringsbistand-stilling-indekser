60ac8ad4dfbfa148ab7e16ad411a24b7509534e3
André Mossige <mossige.a@gmail.com>
2021-01-12 12:53:29 +0100

    Legg testdata i egen fil

---
+++ src/test/kotlin/rekrutteringsbistand/stilling/indekser/kafka/TestData.kt
@@ -0,0 +1,49 @@
+package rekrutteringsbistand.stilling.indekser.kafka
+
+import no.nav.pam.ad.ext.avro.*
+import java.time.LocalDateTime
+
+val enAd = Ad(
+    "uuid",
+    "tittel",
+    AdStatus.ACTIVE,
+    PrivacyChannel.INTERNAL_NOT_SHOWN,
+    Administration(
+        AdministrationStatus.DONE,
+        listOf(RemarkType.FOREIGN_JOB),
+        "kommentar",
+        "reportee",
+        "navIdent"
+    ),
+    LocalDateTime.now().toString(),
+    LocalDateTime.now().toString(),
+    LocalDateTime.now().toString(),
+    LocalDateTime.now().toString(),
+    Company(
+        "navn",
+        "publicname",
+        "orgnr",
+        "parentOrgnr",
+        "orgform"
+    ),
+    listOf(StyrkCategory("kode", "name")),
+    "source",
+    "medium",
+    "reference",
+    LocalDateTime.now().toString(),
+    "businessName",
+    listOf(
+        Location(
+            "address",
+            "postalCode",
+            "county",
+            "municipal",
+            "country",
+            "latitue",
+            "longitude",
+            "municipal_code",
+            "county_code"
+        )
+    ),
+    listOf(Property("key", "value"))
+)
