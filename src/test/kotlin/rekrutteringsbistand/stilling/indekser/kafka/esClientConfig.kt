821f2d4e2151a62190b0230ffdd7f3c7ca5d7546
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-01-15 14:23:40 +0100

    Øk timeout for kobling til ES

--- src/test/kotlin/rekrutteringsbistand/stilling/indekser/kafka/esClientConfig.kt
+++ src/test/kotlin/rekrutteringsbistand/stilling/indekser/kafka/esClientConfig.kt
@@ -1,6 +1,7 @@
 package rekrutteringsbistand.stilling.indekser.kafka
 
 import org.apache.http.HttpHost
+import org.apache.http.client.config.RequestConfig
 import org.elasticsearch.client.RestClient
 import org.elasticsearch.client.RestHighLevelClient
 import rekrutteringsbistand.stilling.indekser.utils.environment
@@ -8,5 +9,12 @@ import rekrutteringsbistand.stilling.indekser.utils.environment
 fun getLocalEsClient(): RestHighLevelClient {
     val url = environment().get("ELASTIC_SEARCH_API")
 
-    return RestHighLevelClient(RestClient.builder(HttpHost.create(url)))
+    return RestHighLevelClient(RestClient.builder(HttpHost.create(url))
+            .setRequestConfigCallback { requestConfigBuilder: RequestConfig.Builder ->
+                requestConfigBuilder
+                        .setConnectionRequestTimeout(5000)
+                        .setConnectTimeout(10000)
+                        .setSocketTimeout(20000)
+            }
+    )
 }
