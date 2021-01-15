a80316ed046be55c587b32d91b796110eb2c17c0
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-01-15 14:29:53 +0100

    Øk timeout for ES i miljø, ikke lokalt

--- src/main/kotlin/rekrutteringsbistand/stilling/indekser/elasticsearch/esClientConfig.kt
+++ src/main/kotlin/rekrutteringsbistand/stilling/indekser/elasticsearch/esClientConfig.kt
@@ -4,6 +4,7 @@ import org.apache.http.HttpHost
 import org.apache.http.auth.AuthScope
 import org.apache.http.auth.UsernamePasswordCredentials
 import org.apache.http.client.CredentialsProvider
+import org.apache.http.client.config.RequestConfig
 import org.apache.http.impl.client.BasicCredentialsProvider
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder
 import org.elasticsearch.client.RestClient
@@ -21,6 +22,12 @@ fun getEsClient(): RestHighLevelClient {
 
     return RestHighLevelClient(RestClient
             .builder(HttpHost.create(url))
+            .setRequestConfigCallback { requestConfigBuilder: RequestConfig.Builder ->
+                requestConfigBuilder
+                        .setConnectionRequestTimeout(5000)
+                        .setConnectTimeout(10000)
+                        .setSocketTimeout(20000)
+            }
             .setHttpClientConfigCallback { httpAsyncClientBuilder: HttpAsyncClientBuilder ->
                 httpAsyncClientBuilder
                         .setDefaultCredentialsProvider(credentialsProvider)
--- src/test/kotlin/rekrutteringsbistand/stilling/indekser/kafka/esClientConfig.kt
+++ src/test/kotlin/rekrutteringsbistand/stilling/indekser/kafka/esClientConfig.kt
@@ -9,12 +9,5 @@ import rekrutteringsbistand.stilling.indekser.utils.environment
 fun getLocalEsClient(): RestHighLevelClient {
     val url = environment().get("ELASTIC_SEARCH_API")
 
-    return RestHighLevelClient(RestClient.builder(HttpHost.create(url))
-            .setRequestConfigCallback { requestConfigBuilder: RequestConfig.Builder ->
-                requestConfigBuilder
-                        .setConnectionRequestTimeout(5000)
-                        .setConnectTimeout(10000)
-                        .setSocketTimeout(20000)
-            }
-    )
+    return RestHighLevelClient(RestClient.builder(HttpHost.create(url)))
 }
