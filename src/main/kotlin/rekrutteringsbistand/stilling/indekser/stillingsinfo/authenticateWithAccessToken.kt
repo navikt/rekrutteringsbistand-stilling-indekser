3b5895c8005e65a9e04509a37042aeb81b83d0b2
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-03-23 12:05:51 +0100

    Fiks scope til rekbis-api i prod

--- src/main/kotlin/rekrutteringsbistand/stilling/indekser/stillingsinfo/authenticateWithAccessToken.kt
+++ src/main/kotlin/rekrutteringsbistand/stilling/indekser/stillingsinfo/authenticateWithAccessToken.kt
@@ -3,10 +3,14 @@ package rekrutteringsbistand.stilling.indekser.stillingsinfo
 import com.github.kittinunf.fuel.core.FuelManager
 import rekrutteringsbistand.stilling.indekser.autentisering.AccessTokenClient
 import rekrutteringsbistand.stilling.indekser.autentisering.addBearerToken
+import rekrutteringsbistand.stilling.indekser.utils.Environment
 
 fun authenticateWithAccessToken(httpClient: FuelManager, accessTokenClient: AccessTokenClient): FuelManager {
+    val cluster = Environment.get("NAIS_CLUSTER_NAME")
+    val clusterTilRekrutteringsbistandApi = if (cluster === "prod-gcp") "prod-fss" else "dev-fss"
+
     addBearerToken(httpClient) {
-        accessTokenClient.getAccessToken(scope = "api://dev-fss.arbeidsgiver.rekrutteringsbistand-api/.default")
+        accessTokenClient.getAccessToken(scope = "api://$clusterTilRekrutteringsbistandApi.arbeidsgiver.rekrutteringsbistand-api/.default")
     }
 
     return httpClient
