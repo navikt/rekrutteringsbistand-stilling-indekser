d5a3b3e8bb30d2f40bbfc277c5d69ef036bf98c4
André Mossige <andremossige@fastmail.com>
2021-04-06 15:02:14 +0200

    Logg scope for rekrutteringsbistand-stilling-api

--- src/main/kotlin/rekrutteringsbistand/stilling/indekser/stillingsinfo/authenticateWithAccessToken.kt
+++ src/main/kotlin/rekrutteringsbistand/stilling/indekser/stillingsinfo/authenticateWithAccessToken.kt
@@ -4,15 +4,17 @@ import com.github.kittinunf.fuel.core.FuelManager
 import rekrutteringsbistand.stilling.indekser.autentisering.AccessTokenClient
 import rekrutteringsbistand.stilling.indekser.autentisering.addBearerToken
 import rekrutteringsbistand.stilling.indekser.utils.Environment
+import rekrutteringsbistand.stilling.indekser.utils.log
 
 fun authenticateWithAccessToken(httpClient: FuelManager, accessTokenClient: AccessTokenClient): FuelManager {
     val cluster = Environment.get("NAIS_CLUSTER_NAME")
     val clusterTilRekrutteringsbistandApi = if (cluster === "prod-gcp") "prod-fss" else "dev-fss"
+    val scope = "api://$clusterTilRekrutteringsbistandApi.arbeidsgiver.rekrutteringsbistand-stilling-api/.default"
+
+    log("authenticateWithAccessToken").info("Bruker scope $scope")
 
     addBearerToken(httpClient) {
-        accessTokenClient.getAccessToken(
-            scope = "api://$clusterTilRekrutteringsbistandApi.arbeidsgiver.rekrutteringsbistand-stilling-api/.default"
-        )
+        accessTokenClient.getAccessToken(scope)
     }
 
     return httpClient
