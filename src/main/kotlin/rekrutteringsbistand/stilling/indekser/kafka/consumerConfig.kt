f9af526aaf922aaf16af28f830bdc9263b166f71
Joar Aurdal <joar.aurdal@nav.no>
2021-05-06 14:17:04 +0200

    Bytt til aiven consumer

--- src/main/kotlin/rekrutteringsbistand/stilling/indekser/kafka/consumerConfig.kt
+++ src/main/kotlin/rekrutteringsbistand/stilling/indekser/kafka/consumerConfig.kt
@@ -21,20 +21,15 @@ fun consumerConfig(versjon: Int) = Properties().apply {
     put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java)
     put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer::class.java)
 
-    put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, Environment.get("KAFKA_BROKER_URLS"))
-    put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, Environment.get("KAFKA_SCHEMA_REGISTRY_URL"))
+    put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, Environment.get("KAFKA_BROKERS"))
+    put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, Environment.get("KAFKA_SCHEMA_REGISTRY"))
     put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true)
 
-    val serviceuserUsername = Environment.get("SERVICEBRUKER_BRUKERNAVN")
-    val serviceuserPassword = Environment.get("SERVICEBRUKER_PASSORD")
-    val saslJaasConfig = "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${serviceuserUsername}\" password=\"${serviceuserPassword}\";"
-    put(SaslConfigs.SASL_JAAS_CONFIG, saslJaasConfig)
-    put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_PLAINTEXT")
-    put(SaslConfigs.SASL_MECHANISM, "PLAIN")
-
-    System.getenv("NAV_TRUSTSTORE_PATH")?.let {
-        put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL")
-        put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, File(it).absolutePath)
-        put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, System.getenv("NAV_TRUSTSTORE_PASSWORD"))
-    }
+    put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
+    put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "");
+    put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, Environment.get("KAFKA_TRUSTSTORE_PATH"));
+    put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, Environment.get("KAFKA_CREDSTORE_PASSWORD"));
+    put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "PKCS12")
+    put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, Environment.get("KAFKA_KEYSTORE_PATH"));
+    put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, Environment.get("KAFKA_CREDSTORE_PASSWORD"));
 }
