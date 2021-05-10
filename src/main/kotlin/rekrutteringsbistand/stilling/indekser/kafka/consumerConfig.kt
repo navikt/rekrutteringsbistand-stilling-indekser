7cb2afd2d7bb7985a0dc13c6e28480f66fd594d5
Joar Aurdal <joar.aurdal@nav.no>
2021-05-10 15:47:27 +0200

    Legg inn team i topic

--- src/main/kotlin/rekrutteringsbistand/stilling/indekser/kafka/consumerConfig.kt
+++ src/main/kotlin/rekrutteringsbistand/stilling/indekser/kafka/consumerConfig.kt
@@ -9,7 +9,7 @@ import org.apache.kafka.common.serialization.StringDeserializer
 import rekrutteringsbistand.stilling.indekser.utils.Environment
 import java.util.*
 
-const val stillingstopic = "rekrutteringsbistand-stilling-1"
+const val stillingstopic = "arbeidsgiver.rekrutteringsbistand-stilling-1"
 
 fun consumerConfig(versjon: Int) = Properties().apply {
     put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 100)
