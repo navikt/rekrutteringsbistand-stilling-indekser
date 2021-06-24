5c8cabde94ddb8d5eef549f8812d0b47d374d27b
frodank <11638682+frodank@users.noreply.github.com>
2021-06-24 17:32:30 +0200

    Oppgradere til Gradle 7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,20 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.4.21"
-    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
     id("com.github.davidmc24.gradle.plugin.avro") version "1.2.0"
     application
 }
 
 application {
-    mainClassName = "rekrutteringsbistand.stilling.indekser.AppKt"
+    mainClass.set("rekrutteringsbistand.stilling.indekser.AppKt")
 }
 
 repositories {
     jcenter()
+
+    maven {
+        url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    }
 
     maven {
         url = uri("https://packages.confluent.io/maven/")
@@ -25,8 +29,9 @@ dependencies {
     implementation("io.javalin:javalin:3.12.0")
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("net.logstash.logback:logstash-logback-encoder:6.3")
-    implementation("com.github.kittinunf.fuel:fuel:2.3.0")
-    implementation("com.github.kittinunf.fuel:fuel-jackson:2.3.0")
+    val fuelVersion = "2.3.0"
+    implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
+    implementation("com.github.kittinunf.fuel:fuel-jackson:$fuelVersion")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.0")
     implementation("com.github.ben-manes.caffeine:caffeine:2.8.8")
     implementation("org.apache.kafka:kafka-clients:2.7.0")
