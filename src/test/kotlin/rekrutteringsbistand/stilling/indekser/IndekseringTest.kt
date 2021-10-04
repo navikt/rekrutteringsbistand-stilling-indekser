ef37805131acae530673b30c7f41134e876d4312
frodank <11638682+frodank@users.noreply.github.com>
2021-10-04 15:54:49 +0200

    stillingskategori-feltet i Stillingsinfo er endret navn på (Fiks på test)

--- src/test/kotlin/rekrutteringsbistand/stilling/indekser/IndekseringTest.kt
+++ src/test/kotlin/rekrutteringsbistand/stilling/indekser/IndekseringTest.kt
@@ -235,7 +235,7 @@ class IndekseringTest {
                 esClientMock.indekser(forventedeStillinger, hentIndeksNavn(indeksversjon))
             }
 
-            assertEquals("STILLING", forventedeStillinger.first().stillingsinfo?.oppdragKategori)
+            assertEquals("STILLING", forventedeStillinger.first().stillingsinfo?.stillingskategori)
         }
     }
 }
