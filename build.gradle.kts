28700932b181c28d352831032b1a5f6f737f320e
vildestefferud <qwartz.ms@hotmail.com>
2022-10-21 11:07:01 +0200

    Fjern bumping av dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(18))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 application {
@@ -26,23 +26,23 @@ repositories {
 }
 
 dependencies {
-    implementation("org.apache.avro:avro:1.11.0")
+    implementation("org.apache.avro:avro:1.10.1")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("io.javalin:javalin:5.1.1")
-    implementation("ch.qos.logback:logback-classic:1.4.4")
-    implementation("net.logstash.logback:logstash-logback-encoder:7.2")
-    val fuelVersion = "2.3.1"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    implementation("io.javalin:javalin:4.1.1")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("net.logstash.logback:logstash-logback-encoder:6.3")
+    val fuelVersion = "2.3.0"
     implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
     implementation("com.github.kittinunf.fuel:fuel-jackson:$fuelVersion")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
-    implementation("com.github.ben-manes.caffeine:caffeine:3.1.1")
-    implementation("org.apache.kafka:kafka-clients:3.3.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.0")
+    implementation("com.github.ben-manes.caffeine:caffeine:2.8.8")
+    implementation("org.apache.kafka:kafka-clients:2.7.0")
     implementation("io.confluent:kafka-avro-serializer:6.0.1")
-    implementation("org.elasticsearch.client:elasticsearch-rest-high-level-client:7.17.6") // Deprecated, https://www.elastic.co/guide/en/elasticsearch/client/java-api-client/8.4/migrate-hlrc.html
+    implementation("org.elasticsearch.client:elasticsearch-rest-high-level-client:7.10.1")
 
-    testImplementation("io.mockk:mockk:1.13.2")
+    testImplementation("io.mockk:mockk:1.10.5")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
