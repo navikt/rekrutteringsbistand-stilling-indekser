0bdaebd0f1a55f142c212e7f64354ba3bddb1c52
vildestefferud <qwartz.ms@hotmail.com>
2022-10-21 11:27:24 +0200

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,16 +29,16 @@ dependencies {
     implementation("org.apache.avro:avro:1.10.1")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("io.javalin:javalin:4.1.1")
-    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("ch.qos.logback:logback-classic:1.4.4")
     implementation("net.logstash.logback:logstash-logback-encoder:6.3")
-    val fuelVersion = "2.3.0"
+    val fuelVersion = "2.3.1"
     implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
     implementation("com.github.kittinunf.fuel:fuel-jackson:$fuelVersion")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
     implementation("com.github.ben-manes.caffeine:caffeine:2.8.8")
-    implementation("org.apache.kafka:kafka-clients:2.7.0")
+    implementation("org.apache.kafka:kafka-clients:2.7.2")
     implementation("io.confluent:kafka-avro-serializer:6.0.1")
     implementation("org.elasticsearch.client:elasticsearch-rest-high-level-client:7.10.1")
 
