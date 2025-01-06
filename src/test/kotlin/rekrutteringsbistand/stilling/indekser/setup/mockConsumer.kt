e1beee392749b2c6624ac4a08d7108947c1657cd
Are Husby <are.husby@nav.no>
2025-01-06 19:32:18 +0100

    Oppgrader opensearch-rest-high-level-client (#43)

--- src/test/kotlin/rekrutteringsbistand/stilling/indekser/setup/mockConsumer.kt
+++ src/test/kotlin/rekrutteringsbistand/stilling/indekser/setup/mockConsumer.kt
@@ -1,5 +1,6 @@
 package rekrutteringsbistand.stilling.indekser.setup
 
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -12,19 +13,21 @@ import rekrutteringsbistand.stilling.indekser.kafka.stillingstopic
 
 val topic = TopicPartition(stillingstopic, 0)
 
-fun mockConsumer(periodiskSendMeldinger: Boolean = true) = MockConsumer<String, Ad>(OffsetResetStrategy.EARLIEST).apply {
-    schedulePollTask {
-        rebalance(listOf(topic))
-        updateBeginningOffsets(mapOf(Pair(topic, 0)))
+fun mockConsumer(periodiskSendMeldinger: Boolean = true) =
+    MockConsumer<String, Ad>(OffsetResetStrategy.EARLIEST).apply {
+        schedulePollTask {
+            rebalance(listOf(topic))
+            updateBeginningOffsets(mapOf(Pair(topic, 0)))
 
-        if (periodiskSendMeldinger) {
-            GlobalScope.launch {
-                var offset: Long = 0
-                while (!closed()) {
-                    addRecord(ConsumerRecord(stillingstopic, 0, offset++, enAd.getUuid(), enAd))
-                    delay(5_000)
+            if (periodiskSendMeldinger) {
+                @OptIn(DelicateCoroutinesApi::class) // This coroutine should be active for the whole duration of the application's lifetimeo
+                GlobalScope.launch {
+                    var offset: Long = 0
+                    while (!closed()) {
+                        addRecord(ConsumerRecord(stillingstopic, 0, offset++, enAd.getUuid(), enAd))
+                        delay(5_000)
+                    }
                 }
             }
         }
     }
-}
