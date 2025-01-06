567fb2dd82cb202945f3a3f5d16c650c3d849871
Are Husby <are.husby@nav.no>
2025-01-06 18:03:23 +0100

    Oppgrader flere avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,20 +26,20 @@ repositories {
 }
 
 dependencies {
-    implementation("org.apache.avro:avro:1.12.0")
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom")) // Aligns all the Kotlin SDK libraries with the same version
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.apache.avro:avro:1.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
     implementation("io.javalin:javalin:5.1.1")
-    implementation("ch.qos.logback:logback-classic:1.4.4")
-    implementation("net.logstash.logback:logstash-logback-encoder:7.2")
+    implementation("ch.qos.logback:logback-classic:1.5.16")
+    implementation("net.logstash.logback:logstash-logback-encoder:8.0")
     val fuelVersion = "2.3.1"
     implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
     implementation("com.github.kittinunf.fuel:fuel-jackson:$fuelVersion")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.0")
-    implementation("com.github.ben-manes.caffeine:caffeine:3.1.1")
-    implementation("org.apache.kafka:kafka-clients:3.3.1")
-    implementation("io.confluent:kafka-avro-serializer:6.0.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.1")
+    implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
+    implementation("org.apache.kafka:kafka-clients:3.9.0")
+    implementation("io.confluent:kafka-avro-serializer:7.8.0")
     implementation("org.opensearch.client:opensearch-rest-high-level-client:2.3.0")
 
     testImplementation("io.mockk:mockk:1.13.14")
